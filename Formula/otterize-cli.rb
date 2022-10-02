012941e47add7cf4a3c66c92f4c10d9f0538dd55
otterizebot <otterizebot@users.noreply.github.com>
2022-10-02 17:02:29 +0300

    Brew formula update for otterize-cli version v0.1.5

--- Formula/otterize-cli.rb
+++ Formula/otterize-cli.rb
@@ -11,16 +11,16 @@ class OtterizeCli < Formula
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://get.otterize.com/otterize-cli/v0.1.5/otterize-cli_0.1.5_Linux_arm64.tar.gz-cli.zip"
-      sha256 "63f049fef42d6fc07235de32d7546fe1a9f5ff8f18ac8bfc7399c23df6f8dfc6"
+      url "https://get.otterize.com/otterize-cli/v0.1.5/otterize_Linux_arm64.tar.gz-cli.zip"
+      sha256 "a923193f0ced83d59d501bdf01910d6df54e8ef49a7fa0f9021460758aa5116b"
 
       def install
         bin.install "otterize"
       end
     end
     if Hardware::CPU.intel?
-      url "https://get.otterize.com/otterize-cli/v0.1.5/otterize-cli_0.1.5_Linux_x86_64.tar.gz-cli.zip"
-      sha256 "c7326266c68abb4f4fed8e94893e4d039011792e1cd4ecd8d321eaa61a1d8bd3"
+      url "https://get.otterize.com/otterize-cli/v0.1.5/otterize_Linux_x86_64.tar.gz-cli.zip"
+      sha256 "e0fb68bbea2edfa88af823a73c878d5706509500cd6274828bfba0365ba12a36"
 
       def install
         bin.install "otterize"
