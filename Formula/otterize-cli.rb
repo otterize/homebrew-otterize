6add0375655205f9430bd9bddafb98ebb2f27736
otterizebot <otterizebot@users.noreply.github.com>
2023-11-28 17:08:57 +0200

    Brew formula update for otterize-cli version v1.0.2

--- Formula/otterize-cli.rb
+++ Formula/otterize-cli.rb
@@ -5,21 +5,21 @@
 class OtterizeCli < Formula
   desc "Otterize CLI"
   homepage "https://github.com/otterize/otterize-cli"
-  version "1.0.1"
+  version "1.0.2"
   license "Apache-2.0"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://get.otterize.com/otterize-cli/v1.0.1/otterize_darwin_arm64.zip"
-      sha256 "29ee6d3e04ed87f21778eeee6a32e3df5c8157822a15ea02a0a6004a098d8fdd"
+      url "https://get.otterize.com/otterize-cli/v1.0.2/otterize_darwin_arm64.zip"
+      sha256 "045b3faef205b0ea92151b5d9627bac888cb67e9b1412f1e5d0012947eb6d1fe"
 
       def install
         bin.install "otterize"
       end
     end
     if Hardware::CPU.intel?
-      url "https://get.otterize.com/otterize-cli/v1.0.1/otterize_darwin_x86_64.zip"
-      sha256 "7fab6465d76827a4817b6d724b7085b15efe3b3e0bb9e8b31455444fcded4046"
+      url "https://get.otterize.com/otterize-cli/v1.0.2/otterize_darwin_x86_64.zip"
+      sha256 "8f33f3dd6dbee6b7c32574bf09a93844329642142ac487c9bc726c65a0d7dff2"
 
       def install
         bin.install "otterize"
@@ -29,8 +29,8 @@ class OtterizeCli < Formula
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://get.otterize.com/otterize-cli/v1.0.1/otterize_linux_x86_64.tar.gz"
-      sha256 "b15b14aa5f361b6c6eddce7ecac226750a0e215b1c0a0c1a1f2155812ba3c533"
+      url "https://get.otterize.com/otterize-cli/v1.0.2/otterize_linux_x86_64.tar.gz"
+      sha256 "a3ba5d9a9b0e4794f5bc0804c6d1d75d1c4cd242417b101c717a24e40cc92587"
 
       def install
         bin.install "otterize"
